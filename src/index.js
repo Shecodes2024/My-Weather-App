b6ea916db96109da72d18daa9ce48a5fe8a7c732
Loise Musungu <mwongelimusungu@gmail.com>
2024-03-12 12:32:10 +0300

    Injecting the weather emoji from the API

--- src/index.js
+++ src/index.js
@@ -7,14 +7,17 @@ function refreshWeather(response) {
   let windSpeedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
 
+  iconElement.innerHTML = `<img src"${response.data.condition.icon_url}" class="weather-app-icon" />`;
   cityElement.innerHTML = response.data.city;
-
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   timeElement.innerHTML = formatDate(date);
   temperatureElement.innerHTML = Math.round(temperature);
+
+  console.log(response.data);
 }
 
 function formatDate(date) {
